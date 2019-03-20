746aa5f7062b100dc8d1abdcda4a0bc63964e268
Noah Kenn <noah@railwaycabal.com>
2019-03-20 16:18:48 -0500

    Added preliminary polynomial regression model code.

--- anime-model.py
+++ anime-model.py
@@ -1,4 +1,7 @@
 from sklearn import *
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression
 from pandas import *
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
@@ -13,27 +16,30 @@ data.dropna(inplace=True)
 
 data_y = data.iloc[:,7]
 data_x = data.iloc[:, [True, True, True, True, True, True, True, False, True]]
-print(data.columns)
+data_x2 = data_x*data_x+data_x
+print(data_x.columns)
 
-selector = VarianceThreshold()
-selector.fit_transform(data_x)
 
-X2 = sm.add_constant(data_x)
+X2 = sm.add_constant(data_x2)
 est = sm.OLS(data_y, X2)
 est2 = est.fit()
 print(est2.summary())
 
 data_x_train = data_x[:-30000]
 data_x_test = data_x[-30000:]
+data_x2_train = data_x2[:-30000]
+data_x2_test = data_x2[-30000:]
 
 data_y_train = data_y[:-30000]
 data_y_test = data_y[-30000:]
 
+
+
 regr = linear_model.LinearRegression()
 
-regr.fit(data_x_train, data_y_train)
+regr.fit(data_x2_train, data_y_train)
 
-predictions = regr.predict(data_x_test)
+predictions = regr.predict(data_x2_test)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
@@ -42,3 +48,5 @@ print("Mean squared error: %.2f"
       % mean_squared_error(data_y_test, predictions))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(data_y_test, predictions))
+
+
